f40ecb318da8010796994c4692eddcbe2054d61d
juliansimonhoch <julian-hoch@gmx.net>
2023-11-22 21:27:43 +0100

    Initial commit

--- main.js
+++ main.js
@@ -1,95 +1,74 @@
-//IMPORT MODULES
-import './style.css';
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import GUI from 'lil-gui';
-
-//CONSTANT & VARIABLES
-let width = window.innerWidth;
-let height = window.innerHeight;
-//-- GUI PAREMETERS
- 
-//-- SCENE VARIABLES
-var gui;
-var scene;
-var camera;
-var renderer;
-var container;
-var control;
-var ambientLight;
-var directionalLight;
-
-//-- GEOMETRY PARAMETERS
-//Create an empty array for storing all the cubes
- 
-
-function main(){
-  //GUI
+ import './style.css';
+    import * as THREE from 'three';
+    import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
     
-
-  //CREATE SCENE AND CAMERA
-  scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera( 35, width / height, 0.1, 100);
-  camera.position.set(10, 10, 10)
-
-  //LIGHTINGS
-  ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-  scene.add(ambientLight);
-
-  directionalLight = new THREE.DirectionalLight( 0xffffff, 1);
-  directionalLight.position.set(2,5,5);
-  directionalLight.target.position.set(-1,-1,0);
-  scene.add( directionalLight );
-  scene.add(directionalLight.target);
-
-  //GEOMETRY INITIATION
-  // Initiate first cubes
-
-
-  //RESPONSIVE WINDOW
-  window.addEventListener('resize', handleResize);
- 
-  //CREATE A RENDERER
-  renderer = new THREE.WebGLRenderer({alpha:true, antialias:true});
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-  container = document.querySelector('#threejs-container');
-  container.append(renderer.domElement);
-  
-  //CREATE MOUSE CONTROL
-  control = new OrbitControls( camera, renderer.domElement );
-
-  //EXECUTE THE UPDATE
-  animate();
-}
- 
-//-----------------------------------------------------------------------------------
-//HELPER FUNCTIONS
-//-----------------------------------------------------------------------------------
-//RESPONSIVE
-function handleResize() {
-  width = window.innerWidth;
-  height = window.innerHeight;
-  camera.aspect = width / height;
-  camera.updateProjectionMatrix();
-  renderer.setSize(width, height);
-  renderer.render(scene, camera);
-}
-
-//ANIMATE AND RENDER
-function animate() {
-	requestAnimationFrame( animate );
- 
- 
-	renderer.render( scene, camera );
-}
-//-----------------------------------------------------------------------------------
-// CLASS
-//-----------------------------------------------------------------------------------
-
-
-//-----------------------------------------------------------------------------------
-// EXECUTE MAIN 
-//-----------------------------------------------------------------------------------
-
-main();
+    // VARIABLES
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    var geometries = [];
+    let materials = [];
+    var cubes = [];
+    
+    console.log(width, height)
+    
+    // CREATE A SCENE AND CAMERA
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 100)
+    camera.position.set(0, 0, 20)
+    
+    // CREATE GEOMETRIES, MATERIALS, AND CUBES
+    for (let i = 1; i <= 5; i++) {
+        geometries[i] = new THREE.BoxGeometry(i, i, i);
+        materials[i] = new THREE.MeshBasicMaterial({ color: Math.random() * 0xffffff });
+        cubes[i] = new THREE.Mesh(geometries[i], materials[i]);
+        cubes[i].position.set(i, i * i, i);
+    }
+    
+    
+    cubes.forEach(function (element, index) {
+        scene.add(cubes[index]);
+    });
+    
+    // CREATE RENDERER
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    const container = document.querySelector('#threejs-container')
+    container.append(renderer.domElement)
+    
+    // CREATE MOUSE CONTROL
+    const controls = new OrbitControls(camera, renderer.domElement);
+    
+    // HANDLE WINDOW RESIZE
+    function handleResize() {
+        width = window.innerWidth;
+        height = window.innerHeight;
+        camera.aspect = width / height;
+        camera.updateProjectionMatrix();
+        renderer.setSize(width, height);
+        renderer.render(scene, camera);
+    }
+    
+    window.addEventListener('resize', handleResize);
+    
+    // ANIMATE AND RENDER
+    function animate() {
+        requestAnimationFrame(animate);
+    
+        controls.update();
+    
+        // Rotate each cube individually
+        cubes.forEach(function (cube, index) {
+            cube.rotation.x += 0.01 * (index + 1);
+            cube.rotation.y += 0.01 * (index + 1);
+        });
+    
+        renderer.render(scene, camera);
+    }
+    
+    animate();
+    
+    // Handle window resize
+    window.addEventListener('resize', () => {
+        handleResize();
+    });
